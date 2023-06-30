12415f39768527c2cfa67fef15e90bc414792a77
Geronimo <jonnacgeronimo@gmail.com>
2023-06-30 16:12:14 +0200

    ready for correction

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+function Hello() {
+  return <h1>Hello</h1>;
+}
+
+function Message() {
+  return <h2>It's time to learn React!!!</h2>;
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Hello />
+      <Message />
     </div>
   );
 }

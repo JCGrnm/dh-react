ea2f20c49c513d8bfbb351f6facd049176382213
Jonna C. Geronimo <jonnacgeronimo@gmail.com>
2023-07-06 17:53:06 +0200

    done 09

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Welcome from "./components/Welcome";
+
+function App() {
+  return (
+    <>
+      <Welcome name={<strong>Geronimo</strong>} age={26} />
+    </>
+  );
+}
+
+export default App;

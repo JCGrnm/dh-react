9eaf7076ab627080ccf5562db1dac9584f9e46ca
Jonna C. Geronimo <jonnacgeronimo@gmail.com>
2023-07-06 17:44:59 +0200

    done 08

---
+++ src/components/Welcome.js
@@ -0,0 +1,12 @@
+import Age from "./Age";
+
+function Welcome({ name = "World", age }) {
+  return (
+    <>
+      <p>Hello, {name}!!!</p>
+      {age && <Age age={age} />}
+    </>
+  );
+}
+
+export default Welcome;

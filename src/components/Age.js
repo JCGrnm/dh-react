529baf8213ffbef2b0a48690990dd7ce585c77a8
Jonna C. Geronimo <jonnacgeronimo@gmail.com>
2023-07-06 18:03:36 +0200

    done 11

---
+++ src/components/Age.js
@@ -0,0 +1,7 @@
+function Age({ age }) {
+  return (
+    <>{age > 18 ? <p>Your age is {age}</p> : <p>You are very young!</p>}</>
+  );
+}
+
+export default Age;
---
+++ src/components/Welcome.js
@@ -0,0 +1,12 @@
+import Age from "./Age";
+
+function Welcome({ name = "World", age }) {
+  return (
+    <>
+      <p>Hello, {<strong>{name}</strong>}!</p>
+      {age > 18 && age < 65 && name == "John" && <Age age={age} />}
+    </>
+  );
+}
+
+export default Welcome;

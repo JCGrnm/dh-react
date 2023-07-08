641153b0c9a810f181e3fe785280b51101989f8a
Jonna C. Geronimo <jonnacgeronimo@gmail.com>
2023-07-09 01:24:48 +0200

    done 24

---
+++ TodoList.jsx
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+
+class TodoList extends Component {
+  state = {
+    items: [],
+    inputValue: "", //RESET TO ZERO
+  };
+
+  addItem = () => {
+    const { inputValue, items } = this.state;
+    if (inputValue !== "") {
+      this.setState((prevState) => ({
+        items: [...prevState.items, inputValue],
+        inputValue: "", //RESET TO ZERO
+      }));
+    }
+  };
+
+  handleChange = (event) => {
+    this.setState({ inputValue: event.target.value });
+  };
+
+  render() {
+    const { inputValue, items } = this.state;
+
+    return (
+      <div>
+        <h1>STUFF TO DO</h1>
+        <input type="text" value={inputValue} onChange={this.handleChange} />
+        <button onClick={this.addItem}>Add me!</button>
+        <ul>
+          {items.map((item, index) => (
+            <li key={index}>{item}</li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default TodoList;

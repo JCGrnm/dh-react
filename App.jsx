70c39e3057464e88f59cb46cf8074be317083e5c
Jonna C. Geronimo <jonnacgeronimo@gmail.com>
2023-07-09 22:48:14 +0200

    done 36

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { CounterButton } from "./CounterButton";
+
+export class App extends React.Component {
+  render() {
+    return (
+      <>
+        <CounterButton />
+      </>
+    );
+  }
+}

d240bf50e3a2a7d554042efed78fc6f8d650a333
Jonna C. Geronimo <jonnacgeronimo@gmail.com>
2023-07-09 22:32:31 +0200

    done 35

---
+++ GithubUser.jsx
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from "react";
+
+export function GithubUser({ username }) {
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    async function fetchDataGit(username) {
+      try {
+        const response = await fetch(
+          `https://api.github.com/users/${username}`
+        );
+        if (response.ok) {
+          const jsonData = await response.json();
+          setData(jsonData);
+        } else {
+          throw new Error(response.statusText);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    fetchDataGit(username);
+  }, [username]);
+
+  return (
+    <div>
+      <h1>{data.name}</h1>
+      <img src={data.avatar_url} alt="avatar" />
+    </div>
+  );
+}

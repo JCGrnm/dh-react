4d712308075a0587182c743f0da9bc51f8b6f363
Jonna C. Geronimo <jonnacgeronimo@gmail.com>
2023-07-09 11:19:41 +0200

    refactor CounterDisplay.jsx

--- CounterDisplay.jsx
+++ CounterDisplay.jsx
@@ -2,6 +2,6 @@ import React from "react";
 
 export class CounterDisplay extends React.Component {
   render() {
-    return <h1>{this.props.counter}</h1>;
+    return <h1>{this.props.count}</h1>;
   }
 }

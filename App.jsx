fe15fc0c7b54429fdf1c12cdefa18d2583199d3e
Jonna C. Geronimo <jonnacgeronimo@gmail.com>
2023-07-09 22:30:20 +0200

    done 34

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { GithubUser } from "./GithubUser";
+
+export class App extends React.Component {
+  render() {
+    return (
+      <>
+        <GithubUser username="JCGrnm" />
+      </>
+    );
+  }
+}

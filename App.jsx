691b13ba3605b0b148c2a9fd585c10f5824680fe
Jonna C. Geronimo <jonnacgeronimo@gmail.com>
2023-07-09 11:54:23 +0200

    done 28-29

---
+++ App.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { DisplayLanguage } from "./DisplayLanguage";
+import { LanguageContext } from "./LanguageContext";
+
+export class App extends React.Component {
+  state = {
+    language: "EN",
+  };
+
+  render() {
+    return (
+      <>
+        <select
+          onChange={(event) => {
+            this.setState({
+              language: event.target.value,
+            });
+          }}
+        >
+          <option value="EN">English</option>
+          <option value="IT">Italiano</option>
+          <option value="DE">Deutsch</option>
+          <option value="FR">Français</option>
+          <option value="ES">Español</option>
+        </select>
+        <LanguageContext.Provider value={this.state.language}>
+          <DisplayLanguage />
+        </LanguageContext.Provider>
+      </>
+    );
+  }
+}

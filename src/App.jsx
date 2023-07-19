012f25ee113c3e120e44d592421a158e145132b4
Jonna C. Geronimo <jonnacgeronimo@gmail.com>
2023-07-20 01:23:00 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import React, { useState } from "react";
+import React from "react";
 import { Route, Routes } from "react-router-dom";
 import { InteractiveWelcome } from "./InteractiveWelcome";
 import { Counter } from "./Counter";
 
 export const App = () => {
   return (
-      <>
-        <Routes>
-          <Route path="/" element={<InteractiveWelcome />} />
-          <Route path="/counter" element={<Counter initialValue={0} />} />
-        </Routes>
-      </>
+    <>
+      <Routes>
+        <Route path="/" element={<InteractiveWelcome />} />
+        <Route path="/counter" element={<Counter initialValue={0} />} />
+      </Routes>
+    </>
   );
 };
